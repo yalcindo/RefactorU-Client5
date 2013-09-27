70c9691802906854679e7af160a13ed7d6be28e3
Daniel O'Brien <wheresmypen@yahoo.com>
2013-09-27 10:30:54 -0600

    Yalcin review2

--- main.js
+++ main.js
@@ -21,11 +21,25 @@ var getInfo = function(){
 var displayOutput = function(){
 	
 	var i = (data.length - 1);
-	var deleteOutput = $("<div class='view-quotes'><p>"+(i+1)+ "---"+data[i].author + " <br>" + data[i].quote  + 
-		"</p> <button type='button' class='delete-button'>Delete</button></div>");
+	var addOutput = $("<div class='view-quotes'><p>"+(i+1)+ "---"+data[i].author + " <br>" + data[i].quote  + 
+		"</p> <button type='button' class='delete-button'>Delete</button><button type='button' class='rate-button'>Rate</button></div>");
+	// var selectRate = $("<div class='rate-quotes'><button type='button' class='delete-button'>Rate</button></div>");
 	//adds to another item to the output string
 	
-	$(".display-quotes").append(deleteOutput);
+	$(".display-quotes").append(addOutput);
+	// $(".display-quotes").append(selectRate);
+
+	$(".rate-button").mouseover(function(){
+    	$(".rate-button").css("background-color","gray");
+ //    	var hoverMenu = function{
+ //    		var dropMenu = $('<div class="drop"><select><option value=1>1</option></select></div>')
+ //    		$(".display-quotes").append(dropMenu)
+ //    	};
+    	hoverMenu();
+  	});
+  	  $(".rate-button").mouseout(function(){
+    /* NEED TO CLEAR MENU WITH MOUSEOUT;*/
+  });
 	
 	//removes an item when delete is clicked
 	$(".delete-button").click(function(){
@@ -36,6 +50,11 @@ var displayOutput = function(){
 
 
 };
+
+var hoverMenu = function(){
+    		var dropMenu = $('<div class="drop"><select><option value=1>1</option><option value=2>2</option><option value=3>3</option><option value=4>4</option><option value=5>5</option></select></div>')
+    		$(".display-quotes").append(dropMenu)
+    	};
 
 var pullAuthor = function(){
 	var searchAuthor = $("#search-name").val();
